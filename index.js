a5d7cc4abf88c29c0ce273e56154054717235de4
sangmesh wadgave <67791707+sangmesh2450@users.noreply.github.com>
2021-06-01 14:28:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ channelInformation.addEventListener("click",(e)=>{
     dashBoard.innerHTML= `
         <form class="form-inline">
         <div class="form-group mb-2">
-        <h4>Enter the channel Name</h4>
+        <h4>Enter the channel ID</h4>
         </div>
         <div class="form-group mx-sm-3 mb-2">
         <label for="channelSearch" class="sr-only">channelSearch</label>
-        <input type="text" class="form-control" id="channelSearch" placeholder="channelSearch">
+        <input type="text" class="form-control" id="channelSearch" placeholder="Enter the channel ID">
         </div>
         <button type="submit" class="btn channelsearch btn-dark mb-2">Search</button>
     </form>
     `;
-    getChannelDetails(dashBoard);
+     getChannelDetails(dashBoard);
     
 
 });
@@ -29,9 +29,13 @@ uploading.addEventListener("click",(e)=>{
     <button class="btn btn-dark uploadbutton" >Upload</button>
 `;
 });
+
+
+
+
 var search = document.querySelector('.searchVideo');
 search.addEventListener("click",(e)=>{
-    console.log("in the search")
+    e.preventDefault();
     var vidname = document.querySelector(".vidname");
     var dashBoard = document.querySelector(".dashboard");
     var videoname = vidname.value;
@@ -40,7 +44,7 @@ search.addEventListener("click",(e)=>{
     response.then((jsondata)=>{
         return jsondata.json();
     }).then((data)=>{
-       // console.log(data,"data");
+        console.log(data,"data");
         var st=""
         data.items.forEach(element => {
             st+= `
@@ -51,48 +55,48 @@ search.addEventListener("click",(e)=>{
             <p><b>Uploaded on :</b>${element.snippet.publishedAt}</p>
             </div>   
             `
-            console.log(element.snippet.channelId);
+            
         });
         dashBoard.innerHTML=st;
+      
     })
-   // console.log(data.json());  <iframe class="videos" width="420" height ="315" src= "http://www.youtube.com/embed/${element.id.videoId}" frameborder="1" allowfullscreen> </iframe>
+  
 
 });
+
+
 
 function getChannelDetails(dashBoard){
     document.querySelector(".channelsearch").addEventListener("click",(e)=>{
 
-        var videoname = document.querySelector("#channelSearch").value;
+        var channelIdCode = document.querySelector("#channelSearch").value;
         
-        if(videoname.length>0){
+        if(channelIdCode.length>0){
                 
-            var baseUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDhIf5_HQYvNxsMTZo9ptc3iUlJdxx0cIQ&type=video&maxResults=1";
-            var response =  fetch(baseUrl+"&q="+videoname);
+            var baseUrl = "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&key=AIzaSyDhIf5_HQYvNxsMTZo9ptc3iUlJdxx0cIQ";
+            var response =  fetch(baseUrl+"&id="+channelIdCode);
             response.then((jsondata)=>{
                 return jsondata.json();
             }).then((data)=>{
-                console.log(data);
-                data.items.forEach(element => {
-                    var channelid=(element.snippet.channelId);
-                    baseUrl="https://www.googleapis.com/youtube/v3/channels?part=statistics&key=AIzaSyDhIf5_HQYvNxsMTZo9ptc3iUlJdxx0cIQ";
-                    fetch(baseUrl+"&id="+channelid).then((response)=>{
-                        return response.json();
-                    })
-                    .then((jsondata)=>{
-                        console.log(jsondata);
-                        var innerhtml="";
-                        innerhtml+= `
-                        <div class=" videos col-sm col-md col-lg col-xl shadow-lg">
-                        <iframe width= "400px" height ="315px" src= "http://www.youtube.com/embed/${jsondata.items.id}" allowfullscreen></iframe>
-                        <p class="pt-3  "><b>No of subscribers : </b>${jsondata.items[0].statistics.subscriberCount}</p>
-                        <p class=" " ><b>No of videos : </b>${jsondata.items[0].statistics.videoCount}</p>
-                        <pclass=" " ><b>No of views of channel :</b>${jsondata.items[0].statistics.viewCount}</p>
-                        </div>   
-                        `
-                        dashBoard.innerHTML=(innerhtml);
-                    })
-                });
+    
+                element=data.items[0];
+                
+                let innerhtml=`
+                <div class=" videos col-sm col-md col-lg col-xl shadow-lg">
+                <img width="420px" height="310px" src= "${element.snippet.thumbnails.high.url}"></img>
+                <p class="pt-3"><b>Title : </b>${element.snippet.title}</p>
+                <p><b>Channel discription : </b>${element.snippet.description}</p>
+                <p><b>Channel subscribers : </b>${element.statistics.subscriberCount}</p>
+                <p><b>Channel total videos : </b>${element.statistics.videoCount}</p>
+                <p><b>Channel total views : </b>${element.statistics.viewCount}</p>
+                <p><b>Channel Date of upload : </b>${element.snippet.publishedAt}</p>
+                </div>   
+                `;
+
+                dashBoard.innerHTML=innerhtml;
             })
+
         }
-        });
+    });
+
 }
